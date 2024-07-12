1651eee35ee0b62af6d233853b9a9f42a34247dc
Juned1258 <165145520+Juned1258@users.noreply.github.com>
2024-07-12 16:39:04 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "eu-central-1"
-  access_key = "AKIAYS2NWEP6HYDRG2FR"
-  secret_key = "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"
+  access_key = "AKIAYS2NWEP6BJWJX5FH"
+  secret_key = "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"
 }
